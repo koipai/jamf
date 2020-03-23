a4d5fe4402fcc79c39b62b423b73aa5d359b089a
Ryoma Koishi <ryoma.koishi@smarthr.co.jp>
2020-03-23 12:50:35 +0900

    update enroll script

--- script/enroll_script.sh
+++ script/enroll_script.sh
@@ -16,6 +16,9 @@ jamf policy -event install_chrome
 #run policy "Custom_install_googledrivefilestream"
 jamf policy -event install_drivefilestream
 
+#run policy "Custom_install_Zoom"
+jamf policy -event install_zoom
+
 #run policy "Custom_install_Avast"
 jamf policy -event install_avast
 
